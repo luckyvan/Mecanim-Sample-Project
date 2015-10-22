0a20b9e8fd4a8c3881baf406f499172eacf98ebe
Fan Chou <Chou.Fan@igt.com>
2015-10-22 19:47:26 +0800

    7.2.1 control movement by key board. After Normalize(), the speed could be 0, 1, or .9999999.

---
+++ Assets/_Scripts/MyLocomotion.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyLocomotion{
+	private Animator anim;
+	//Animator Parameter Hash Ids
+	private int m_SpeedId = 0;
+	private int m_AngularSpeedId = 0;
+	private int m_DirectionId = 0;
+
+
+	public float m_SpeedDampTime = .1f;
+	public float m_AngularSpeedDampTime = .25f;
+	public float m_DirectionResponseTime = .2f;
+
+	public MyLocomotion(Animator animator){
+		anim = animator;
+
+		m_SpeedId = Animator.StringToHash("Speed");
+		m_AngularSpeedId = Animator.StringToHash ("AngularSpeed");
+		m_DirectionId = Animator.StringToHash ("Direction");
+	}
+
+	public void Do(float speed, float direction){
+		AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo (0);
+		bool inTransition = anim.IsInTransition (0);
+		bool inIdle = state.IsName ("Locomotion.Idle");
+		bool inTurn = state.IsName ("Locomotion.TurnOnSpot");
+		bool inWalkRun = state.IsName ("Locomotion.WalkRun");
+
+		//DampTimes
+		float speedDampTime = inIdle?0:m_SpeedDampTime;
+		float angularSpeedDampTime = inWalkRun || inTransition ? m_AngularSpeedDampTime : 0; //use to decide the walk or run blend tree
+		float directionDampTime = inTurn || inTransition ? 1000000 : 0; //Directly Set the direction, let animation decide the turn speed itself
+
+		float angularSpeed = direction/m_DirectionResponseTime;
+
+		anim.SetFloat (m_SpeedId, speed, speedDampTime, Time.deltaTime);
+		anim.SetFloat (m_AngularSpeedId, angularSpeed, angularSpeedDampTime, Time.deltaTime);
+		anim.SetFloat (m_DirectionId, direction, directionDampTime,Time.deltaTime);
+	}
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Animator anim;
+	private MyLocomotion loc = null;
+	private float speed = 0;
+	private float direction = 0;
+	public bool hasLog = false;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		anim.logWarnings = false;
+		loc = new MyLocomotion(anim);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (anim && Camera.main) {
+			Vector3 dir = new Vector3(Input.GetAxis ("Horizontal"), 0 ,Input.GetAxis ("Vertical"));
+			Vector3 worldDir = Camera.main.transform.rotation * dir; //make it towards camera direction
+			worldDir.y = 0;
+			worldDir.Normalize();
+			float _speed = Mathf.Clamp (worldDir.magnitude, 0, 1);
+			float _direction = 0.0f;
+			if (_speed > .01f) { // moving
+				Vector3 axis = Vector3.Cross (transform.forward, worldDir);
+				_direction = Vector3.Angle (transform.forward, worldDir)/180.0f * (axis.y < 0 ? -1 : 1);
+				speed = _speed;
+				direction = _direction;
+			}
+			loc.Do (speed * 6, direction * 180);
+			anim.SetBool ("HoldLog", hasLog);
+		}
+	}
+}
