a2f8246df8784fc02b557c4ef81b605f5f1ac738
Fan Chou <fanchou1215@gmail.com>
2015-10-25 14:08:47 +0800

    7.2.5
    Player Fall Control. Though I think it should stop the animation.

---
+++ Assets/_Scripts/PlayerFall.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFall : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -5) {
+			transform.position = new Vector3(0, 25, 3);
+		}
+	}
+}
