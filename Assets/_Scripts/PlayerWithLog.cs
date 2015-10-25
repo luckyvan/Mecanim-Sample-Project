c74ca1f4fdd4d10911e3451b6b8e91283bbfbde7
Fan Chou <fanchou1215@gmail.com>
2015-10-25 13:50:36 +0800

    7.2.4
    Add Unity_Log and it's scale control.

---
+++ Assets/_Scripts/PlayerWithLog.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWithLog : MonoBehaviour {
+	public Transform hollowLog;
+	public Transform leftHandle;
+	public Transform rightHandle;
+
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetLayerWeight (1, 1);
+		bool isHolding = anim.GetCurrentAnimatorStateInfo (1).IsName ("HoldLog.HoldLog");
+		hollowLog.localScale = isHolding ?
+			new Vector3 (.2f, .2f, .4f) :
+				new Vector3 (.0001f, .0001f, .0001f);
+	}
+}
