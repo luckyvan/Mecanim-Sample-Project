2de54a1cbafacdf55f1dc780e08c1fbccb8bdad3
Fan Chou <fanchou1215@gmail.com>
2015-10-25 14:04:37 +0800

    7.2.4
    Add left and right hand IK weight, otherwise the hand itself won't move
    with the log.

--- Assets/_Scripts/PlayerWithLog.cs
+++ Assets/_Scripts/PlayerWithLog.cs
@@ -20,4 +20,22 @@ public class PlayerWithLog : MonoBehaviour {
 			new Vector3 (.2f, .2f, .4f) :
 				new Vector3 (.0001f, .0001f, .0001f);
 	}
+
+	void OnAnimatorIK(int layerIndex){
+		if (!enabled) {
+			return;
+		}
+		if (layerIndex == 1) { //HoldLog Layer
+			float ikWeight = anim.GetCurrentAnimatorStateInfo (1).IsName ("HoldLog.HoldLog") ? 1 : 0;
+			anim.SetIKPosition(AvatarIKGoal.LeftHand, leftHandle.transform.position);
+			anim.SetIKRotation(AvatarIKGoal.LeftHand, leftHandle.transform.rotation);
+			anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, ikWeight);
+			anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, ikWeight);
+
+			anim.SetIKPosition(AvatarIKGoal.RightHand, leftHandle.transform.position);
+			anim.SetIKRotation(AvatarIKGoal.RightHand, leftHandle.transform.rotation);
+			anim.SetIKPositionWeight(AvatarIKGoal.RightHand, ikWeight);
+			anim.SetIKRotationWeight(AvatarIKGoal.RightHand, ikWeight);
+		}
+	}
 }
